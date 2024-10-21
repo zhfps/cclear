0d25d42ab94d7e9835fc65288168d71c15a2ff46
zhfps <it_dog_zhang@163.com>
2024-10-21 20:50:59 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import Greet from './components/Greet.vue'
 <template>
   <div class="container">
     <h1>Welcome to Tauri!</h1>
-
     <div class="row">
       <a
         href="https://vitejs.dev"
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       includePaths: ['node_modules/bootstrap/scss']
+  //     }
+  //   }
+  // },
   plugins: [vue()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
