1e9750a603faa03ddd83346713354a8a77f0dbdc
zhfps <it_dog_zhang@163.com>
2024-10-21 21:30:43 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,17 @@
+<script lang="ts" setup>
+  import { NButton, NCard, NConfigProvider, NSpace, lightTheme } from 'naive-ui'
+</script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-    <div class="row">
-      <a
-        href="https://vitejs.dev"
-        target="_blank"
-      >
-        <img
-          src="/vite.svg"
-          class="logo vite"
-          alt="Vite logo"
-        >
-      </a>
-      <a
-        href="https://tauri.app"
-        target="_blank"
-      >
-        <img
-          src="/tauri.svg"
-          class="logo tauri"
-          alt="Tauri logo"
-        >
-      </a>
-      <a
-        href="https://vuejs.org/"
-        target="_blank"
-      >
-        <img
-          src="./assets/vue.svg"
-          class="logo vue"
-          alt="Vue logo"
-        >
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
+  <div class="c-container">
+    <n-config-provider :theme="lightTheme">
+      <n-card>
+        <n-space>
+          <n-button type="primary">
+            浅色
+          </n-button>
+        </n-space>
+      </n-card>
+    </n-config-provider>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 
 import { createApp } from 'vue'
+import 'vfonts/FiraCode.css'
+import './styles/index.scss'
 import App from './App.vue'
 
 const app = createApp(App)
